949e8d968e39b263bff1145003c0a1ebc99f00b6
Greg Merrill <greg.merrill@roostergrin.com>
2021-12-30 11:52:24 -0800

    connect backend and frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -7,19 +7,27 @@ db = SQLAlchemy()
 from .commands import reset_items
 
 def create_app():
-  app = Flask(__name__)
+  app = Flask(__name__, static_folder='build')
 
   uri = os.getenv('DATABASE_URL')
   if uri.startswith('postgres://'):
     uri = uri.replace('postgres://', 'postgresql://', 1)
-
   app.config['SQLALCHEMY_DATABASE_URI'] = uri
-  
+
   db.init_app(app)
 
   from .views import api
   app.register_blueprint(api)
 
+  # Serve React App
+  @app.route('/', defaults={'path': ''})
+  @app.route('/<path:path>')
+  def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+      return send_from_directory(app.static_folder, path)
+    else:
+      return send_from_directory(app.static_folder, 'index.html')
+  
   app.cli.add_command(reset_items)
 
   return app
